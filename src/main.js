a916b3b2dcae33a0db3206ae9ae95b9e5d27dd9c
Andrés Rojas <william.rojas@globant.com>
2025-10-01 16:09:33 +0200

    guardar en localstorage

--- src/main.js
+++ src/main.js
@@ -7,17 +7,38 @@ function createTask() {
   let task = document.getElementById('task').value;
   let description = document.getElementById('description').value;
   let divList = document.getElementById('list');
+
+  let taskContainer = document.createElement("div");
+
   let liTask = document.createElement("dt");
-  liTask.textContent = task;
+  liTask.textContent = "Tarea: " + task;
+
   let liDescription = document.createElement("dd");
-  liDescription.textContent = description;
-  divList.appendChild(liTask);
-  divList.appendChild(liDescription);
+  liDescription.textContent = "Descripción: " + description;
+
+
+  let stateTask = document.createElement("div");
+  stateTask.textContent="Pendiente";
+
+  let state = stateTask.textContent;
+
+
+  taskContainer.appendChild(liTask);
+  taskContainer.appendChild(liDescription);
+  taskContainer.appendChild(stateTask);
+
+
+  divList.appendChild(taskContainer);
+
+  let taskValue = {tarea : task, descripccion : description, estado: state};
+  
+let taskLocalStorage = JSON.parse(localStorage.getItem("taskLocalStorage")) || [];
+taskLocalStorage.push(taskValue);
+localStorage.setItem("taskLocalStorage", JSON.stringify(taskLocalStorage));
 
 }
 
-document.addEventListener('DOMContentLoaded', () => {
+
+
   let addTaskButton = document.getElementById('addTask');
   addTaskButton.addEventListener('click', createTask);
-});
-
